750419f990130da5e83e61b888a5ddd930a769e8
SNIAPA <45017804+HubertWasilewski@users.noreply.github.com>
2021-01-13 20:26:46 +0100

    first

---
+++ Bot.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+const auth = require("./auth.json");
+const client = new Discord.Client();
+const sample = require('lodash.sample');
+const Words = ["https://discord.gg/"];
+client.on("ready", () => {
+ console.log("Zabij się");
+});
+
+
+//randomkick
+client.on('message', message => {
+  // If the message is "!randomkick"
+  if (message.content === '!randomkick') {
+    var members = message.guild.members.fetch();
+    console.log(members.keys);
+    var user = message.guild.members.random();
+    console.log(`${user.user}`);
+  }
+
+});
+
+
+
+
+client.login(auth.token);
